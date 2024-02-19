7d960b3a79934b8197aacc394d22f0a27118989b
Martinos <martinosgeorgiou@gmail.com>
2024-02-19 11:00:26 +0200

    Delete Icons Folder

--- src/assets/Icons/index.jsx
+++
@@ -1,9 +0,0 @@
-import { BsDisc, BsEnvelopeAt } from "react-icons/bs";
-import { GiCompactDisc } from "react-icons/gi";
-import { IoDiscOutline } from "react-icons/io5";
-import { SiDiscogs } from "react-icons/si";
-
-export {
-  BsDisc,
-  GiCompactDisc,
-}
