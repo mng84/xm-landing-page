62a2445a31c810165a43e59b10a7ed6bd24a2d63
Martinos <martinosgeorgiou@gmail.com>
2024-03-05 12:41:28 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Gallery from "./components/Gallery"
 import BigFairHuman from "./components/BigFairHuman"
 import Footer from "./components/Footer"
 
-
 function App() {
   return (
     <div className="w-full h-full max-w-[1200px] m-auto">
